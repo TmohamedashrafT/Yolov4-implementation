b110aa60546c53d7d3f2c09c09273eb9b94d454f
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2023-12-11 21:47:02 +0200

    Update Training.py

--- Training.py
+++ Training.py
@@ -1,94 +1,110 @@
-from Config import cfg
 from utils  import Early_stopping
 from Compute_loss import ComputeLoss
 from Dataset_Generator import data_loader
 from torch.optim import lr_scheduler
-from eval import eval_
-import tqdm
-
+from Yolov4_architecture.model import Yolov4 
+from eval_ import eval_
+from tqdm import tqdm
+import torch
+from Config import cfg
+from utils import read_yaml
+import argparse
+try:
+    from apex import amp
+except:
+    amp = None
 
 class Training:
-  def __init__(self,weight_path=None,device='cpu'):
-    self.device=device
-    self.best_map=0
-    self.batch_size = cfg.model.batch_size
-    self.model=Yolov4(cfg.model.num_classes,
-             cfg.model.stride,
-             cfg.model.anchors,
-             cfg.model.feature_channels,
-             ).to(device='cuda')
-    self.optimizer = self.optimizer_( 
-                                cfg.model.optimizer, 
-                                cfg.model.lr , 
-                                cfg.model.momentum, 
-                                cfg.model.weight_decay)
-    self.eatly_stopping= Early_stopping(patience=cfg.model.patience)
-    if weight_path and weight_path[-3:]=='.pt':
-      self.load_model(weight_path)
-    self.compute_loss = ComputeLoss(device=cfg.model.device,
-                                    num_classes=cfg.model.num_classes, 
-                                    anchors    =cfg.model.anchors,
-                                    balance    =cfg.model.head_scales,
-                                    iou_thresh =cfg.model.iou_thresh ,
-                                     )
-    self.train_loader,_= data_loader(img_dir=cfg.model.train_image_path
-                              ,ann_dir=cfg.model.train_ann_path
-                              ,anchors=cfg.model.anchors,
-                              iou_thresh=cfg.model.iou_wh_anchor,
-                              batch_size=self.batch_size,
-                              data_aug=cfg.model.data_aug_train)
-    self.val_loader ,self.count_classes = data_loader(img_dir=cfg.model.val_image_path,
-                                   ann_dir=cfg.model.val_ann_path,
-                                   anchors=cfg.model.anchors,
-                                   iou_thresh=cfg.model.iou_wh_anchor,
-                                   batch_size=self.batch_size,
-                                   train=False,
-                                   data_aug=cfg.model.data_aug_val,
-                                  
-                                   )
-    self.start_epoch = cfg.model.start_epoch
-    self.epochs      = cfg.model.epochs
-    self.accumulate  = 2
-    self.classes     = dict(enumerate(cfg.model.classes)) 
-    self.weights_path= cfg.model.weights_path
-    self.img_size    = cfg.model.img_size
-    self.freeze      = cfg.model.freeze
-    lf = lambda x: (1 - x / 100) * (1.0 - cfg.model.lr) + cfg.model.lr
-    self.scheduler = lr_scheduler.LambdaLR(self.optimizer, lr_lambda=lf)  
+  def __init__(self, cfg, aug):
+    self.device   = 'cuda' if torch.cuda.is_available() else 'cpu'
+    self.best_map = 0
+    self.batch_size = cfg['batch_size']
+    self.anchors = cfg['anchors']
+    self.model = Yolov4(num_classes = cfg['num_classes'],
+                        anchors = cfg['anchors'],
+                        device  = self.device,
+                        ).to(device = self.device)
+    self.optimizer = self.optimizer_(
+                        cfg['optimizer'],
+                        cfg['lr'] ,
+                        cfg['momentum'],
+                        cfg['weight_decay'])
+    self.eatly_stopping= Early_stopping(patience = cfg['patience'])
+    if  cfg['pretrained'] and  cfg['weights'][-3:]=='.pt':
+      self.load_model(cfg['weights'])
+    self.train_loader,_= data_loader(img_dir =cfg['train_image_path'],
+                                    ann_dir  = cfg['train_ann_path'],
+                                    anchors  = cfg['anchors'],
+                                    img_size = cfg['img_size'],
+                                    data_aug = False,
+                                    cfg_aug  = aug,
+                                    max_boxes=cfg['max_boxes'],
+                                    num_classes   = cfg['num_classes'],
+                                    anchor_thresh = cfg['anchor_thresh'] , 
+                                    batch_size = cfg['batch_size'],
+                                    strides    = cfg['strides'],
+                                    train = True
+                              )
+           
+    self.val_loader ,self.count_classes = data_loader(img_dir    = cfg['val_image_path'],
+                                                      ann_dir    = cfg['val_ann_path'],
+                                                      anchors    = cfg['anchors'],
+                                                      img_size   = cfg['img_size'],
+                                                      num_classes= cfg['num_classes'],
+                                                      batch_size = cfg['batch_size'],
+
+                                                    )
+    self.epochs      = cfg['epochs']
+    self.classes     = dict(enumerate(cfg['classes']))
+    self.weights_path  = cfg['weights']
+    self.img_size    = cfg['img_size']
+    self.ComputeLoss = ComputeLoss(self.device, cfg['num_classes'],cfg['img_size'])
+    self.fp16        = cfg['fp16']
+    lf = lambda x: (1 - x / 100) * (1.0 - cfg['lr']) + cfg['lr']
+    self.scheduler = lr_scheduler.LambdaLR(self.optimizer, lr_lambda = lf)  
   def train(self):
-    self.Freeze()
-    #mloss = torch.zeros(3, device=self.device)
     print('################ Start  Training ################')
-    for epoch in range(self.start_epoch,self.epochs):
+    if self.fp16 and amp:
+        self.model, self.optimizer = amp.initialize(self.yolov4, self.optimizer, opt_level="O1")
+    for epoch in range(self.epochs):
       self.model.train()
-      self.optimizer.zero_grad()
-
-      for i,(imgs,targets,all_boxes) in enumerate(tqdm(self.train_loader)):
-        imgs  = imgs.to(device=self.device,non_blocking=True).float()/255
-        pred = self.model(imgs)
-        loss,loss_item = self.compute_loss(pred,targets,all_boxes)  
-        
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=5.0)
-        self.optimizer.step()  
+      box_loss = 0
+      obj_loss = 0
+      cls_loss = 0
+      for i,(imgs, targets) in enumerate(tqdm(self.train_loader, ascii = True, desc ="Training")):
+        imgs  = imgs.to(device=self.device, non_blocking=True).float() / 255
+        preds = self.model(imgs)
+        loss, loss_item =  self.ComputeLoss(preds, targets)
+        box_loss   += loss_item[0].cpu().numpy()
+        obj_loss   += loss_item[1].cpu().numpy()
+        cls_loss   += loss_item[2].cpu().numpy()
         self.optimizer.zero_grad()
-
-      print(f'epoch {epoch}/{self.epochs} box loss = ',loss_item[0].cpu().numpy(),'  conf loss = ',loss_item[1].cpu().numpy(),'  cls loss = ',loss_item[2].cpu().numpy(),'\n')
-      map, maps  =eval_(device='cuda',
-                            img_size=self.img_size,
-                            model=self.model,
-                            val_loader=self.val_loader,
-                            classes=self.classes,
-                            count_classes=self.count_classes)
-      
+        if self.fp16 and amp:
+             with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                    scaled_loss.backward()
+        else:
+             loss.backward()
+                
+        self.optimizer.step()
+        self.optimizer.zero_grad()
       self.scheduler.step()
+      self.scheduler.get_last_lr()
+      print(f'epoch {epoch}/{self.epochs} box loss = ',box_loss,'  conf loss = ',obj_loss,'  cls loss = ',cls_loss,'\n')
+      map, maps  =eval_(device      = self.device,
+                            batch_size = self.batch_size,
+                            img_size   = self.img_size,
+                            model      = self.model,
+                            val_loader = self.val_loader,
+                            classes    = self.classes,
+                            count_classes = self.count_classes)
+
       self.save_model(epoch,map[0],map[1])
-      ### early stopping 
+      ### early stopping
       if self.eatly_stopping(self.weighted_map(map[0],map[1])):
         print('################ early_stopping ################')
         break
-      ### end epoch 
-    print('################ Training Finished ################')  
+      ### end epoch
+    print('################ Training Finished ################')
 
 
   def weighted_map(self,map50,map50_95,Wmap50=0.1,Wmap50_95=0.9):
@@ -97,8 +113,11 @@ class Training:
 
   def load_model(self,weights_path):
       ckpt = torch.load(weights_path, map_location=self.device)
-      self.model.load_state_dict(ckpt['weights']) 
+      self.model.load_state_dict(ckpt['weights'])
       self.optimizer.load_state_dict(ckpt['optimizer'])
+      #self.best_map50_95=ckpt(['mAP50-95'])
+      #self.best_map50   = ckpt(['mAP50'])
+      self.best_map  = 0
       del ckpt
 
   def save_model(self, epoch,map50,map50_95):
@@ -107,22 +126,18 @@ class Training:
             'mAP50':map50,
             'mAP50-95' :map50_95,
             'optimizer':self.optimizer.state_dict(),
-            'epoch':epoch
+            'epoch':epoch,
+            'anchors':self.anchors,
+             'classes': self.classes
             }
       torch.save(ckpt, self.weights_path+'last.pt')
       if map_>self.best_map:
           self.best_map=map_
           torch.save(ckpt, self.weights_path+'best.pt')
       del ckpt
-
-  def Freeze(self,):
-    freeze = [f'model.{x}.' for x in (self.freeze if len(self.freeze) > 1 else range(self.freeze[0]))]  # layers to freeze
-    for k, v in self.model.named_parameters():
-        v.requires_grad = True  
-        if any(x in k for x in freeze): 
-            v.requires_grad = False
+  
   def optimizer_(self,opt_name='RMSProp',lr=0.001,momentum=0.9,weight_decay=1e-5):
-      if opt_name=='SGD':
+      if   opt_name=='SGD':
           optimizer = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=momentum,weight_decay=weight_decay)
       elif opt_name=='Adam':
           optimizer = torch.optim.Adam(self.model.parameters(), lr=lr, betas=(momentum,0.999),weight_decay=weight_decay)
@@ -131,4 +146,31 @@ class Training:
       else:
           raise NotImplementedError(f'optimizer {opt_name} not implemented')
       return optimizer
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", type = str, default = "data/BBCD.yaml", help = "data.yaml")  
+    parser.add_argument("--hyp", type = str, default = "data/hyp.yaml", help = "hyp.yaml")  
+    parser.add_argument("--aug", type = str,  default = "data/aug.yaml", help = "augmentation file")  
+    parser.add_argument("--pretrained", type = bool, default = False, help = "Resume trainin")  
+    parser.add_argument("--weights", type = str, default = "", help = "weights path")  
+    parser.add_argument("--epochs", type = int, default = 100, help = "number of epochs")  
+    parser.add_argument("--fp16", type = bool, default = False, help = "FP16 Training")  
+    opt = parser.parse_args()
+    data, hyp, aug =  read_yaml(opt.data), read_yaml(opt.hyp), read_yaml(opt.aug)
+    cfg = dict()
+    cfg.update(data)
+    cfg.update(hyp)
+    cfg['pretrained'] = opt.pretrained
+    cfg['weights']    = opt.weights
+    cfg['epochs']     = opt.epochs
+    cfg['anchors']    = torch.tensor(cfg['anchors']).view(3,3,2)
+    cfg['fp16']       = opt.fp16
+    train = Training(cfg, aug)
+    train.train()
+
 
