e0310c6939aed227bcefa379021268639c449912
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2023-12-11 21:22:36 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,21 +1,24 @@
-from utils import NMS,load_image
+from utils import NMS, load_image, draw_boxes
 import torch
 import os
 import numpy as np
 import cv2
-
-def pred_without_eval(img_path='',
-                      model=None,
-                      conf_thresh=0.1,
-                      iou_thresh=0.6,
-                      classes=[],
-                      save_dire='/content',
-                      device='cuda',
-                      weights=None,
-                      img_size=416):
+from Yolov4_architecture.model import Yolov4    
+import time
+def pred(img_path = '',
+         conf_thresh = 0.1,
+         iou_thresh  = 0.45,
+         classes = [],
+         device  = 'cuda',
+         weights = None,
+         img_size= 224):
   if weights:
-    ckpt=torch.load(weights,map_location='cpu')
+    ckpt  = torch.load(weights,map_location='cpu')
+    model = Yolov4(num_classes = len(ckpt['classes']),
+                   anchors = ckpt['anchors'],
+                    device = device,).to(device =device)
     model.load_state_dict(ckpt['weights'])
+    classes = ckpt['classes'] 
     del ckpt 
   img, (oldh, oldw), (newh,neww),scale,(dw,dh)=load_image(img_path,img_size)
   shapes  = scale, (dw,dh)
@@ -23,25 +26,28 @@ def pred_without_eval(img_path='',
   img = np.ascontiguousarray(img)
   img = torch.from_numpy(img[None])
   img = img.to(device, non_blocking=True).float()/255
+  t0 = time.time()
   with torch.no_grad():
      preds=model(img)
-    
+  t1 = time.time()
   preds = NMS(preds,conf_thresh,iou_thresh)
+  t2 = time.time()
+  print('prediction speed = ',t1 - t0,'NMS speed = ',t2 - t1) 
   draw_boxes(img_path,preds,shapes,classes,save_dire)
-def draw_boxes(img_path,preds,shapes,classes,save_dir=''):
-  img = cv2.imread(img_path)    
-  preds=preds[0].cpu().numpy()
-  for pred in preds:
-    #print(pred.shape)
-    if not pred.shape[0]:
-      continue
-    if len(classes) == 1:
-      pred[5] = 0
-    box = scale_boxes( pred[:4], shapes)
-    img = cv2.putText(img,str(classes[int(pred[5])])+' '+str(np.round(pred[4],2)),(int(box[0]), int(box[1]-10)),cv2.FONT_HERSHEY_SIMPLEX,1, (255,0,0), 2, cv2.LINE_AA)
-    img = cv2.rectangle(img,(int(box[0]),int(box[1])),(int(box[2]),int(box[3])),color=(0,0,255),thickness=2)
- 
-  di=img_path.strip().split('/')[-1]
-  if save_dir[-1]!='/':
-    save_dir=save_dir+'/' 
-  cv2.imwrite(save_dir+di, img) 
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--weights", type = str, default = "weights/best.pt", help = "weights path")  
+    parser.add_argument("--src", type = str, default = "img_test/img1.jpg", help = "source")  
+    parser.add_argument("--conf_thresh", type = float,  default = 0.01, help = "confidence threshold")  
+    parser.add_argument("--iou_thresh", type = float, default = 0.45, help = "NMS threshold")  
+    parser.add_argument("--img_size", type = int, default = 224, help = "image size")  
+    opt = parser.parse_args()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    pred(img_path = opt.src,
+         conf_thresh = opt.conf_thresh,
+         iou_thresh  = opt.iou_thresh,
+         device  = 'cuda',
+         weights = opt.weights,
+         img_size= opt.img_size)
